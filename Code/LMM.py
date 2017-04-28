7c27b5ef042d75b7352d5d02a0a8410093d3ab40
Haiying Liang <Haiying Liang>
2017-04-28 15:08:50 -0700

    changes to Probit

--- Code/LMM.py
+++ Code/LMM.py
@@ -10,9 +10,11 @@ from scipy.stats import norm
 from copy import deepcopy
 import matplotlib.pyplot as plt
 
-N = 200     # observations per group
-K = 5      # dimension of regressors
-NG = 200      # number of groups
+# np.random.seed(123242)
+
+N = 1     # observations per group
+K = 1      # dimension of regressors
+NG = 1      # number of groups
 
 
 
@@ -20,13 +22,21 @@ NG = 200      # number of groups
 NObs = NG * N
 
 # variances
-prior_var = {'beta':100, 'mu':100, 'y':1}
+prior_var = {'beta':10, 'mu':100, 'y':1}
 
-beta = np.random.multivariate_normal(np.zeros(K), np.identity(K))
+beta = np.random.multivariate_normal(np.ones(K)*5, np.identity(K))
+#beta = np.arange(K)
+#beta = beta - np.mean(beta)
 
-mu = np.random.normal(0, 1, NG)
+mu = np.random.normal(10, 20, NG)
 
-X = np.random.random(K * NObs).reshape(K, NObs) - 0.5
+# X = np.random.random(K * NObs).reshape(K, NObs) - 0.5
+#x_mat = np.random.random(K * NObs).reshape(NObs, K) - 0.5
+X = np.identity(1)
+
+# try with correlated design matrix
+
+
 y_g_vec = np.array([ g for g in range(NG) for n in range(N) ])
 y_mean = np.dot(X.T, beta) + mu[y_g_vec]
 
@@ -41,7 +51,7 @@ beta_var = np.linalg.inv(1/prior_var['beta']*np.identity(K) \
 mu_mean = np.random.normal(0,1, NG)
 mu_var = (1/prior_var['mu'] + N/prior_var['y'])**(-1)
 
-iterations = 20
+iterations = 10
 beta_error = np.zeros(iterations+1)
 mu_error = np.zeros(iterations+1)
 
@@ -52,7 +62,7 @@ for i in range(iterations):
     [beta_mean, mu_mean] = lmm_CAVI(X, y_vec, beta_mean, beta_var, mu_mean, mu_var, prior_var, NG,N,K)
     beta_error[i+1] = np.linalg.norm(beta_mean - beta)
     mu_error[i+1] = np.linalg.norm(mu_mean - mu)
-    
+    print(beta_mean)
     
 plt.figure(1)
 plt.clf()
